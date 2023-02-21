25546be334f8ca8f220e15bde624d012674afd57
Syntaxbullet <syntaxbullet@protonmail.com>
2023-02-22 00:24:33 +0100

    chore: formatting fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1 class="m-4 bg-blue-900 text-blue-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>This is another paragraph</p>
+
