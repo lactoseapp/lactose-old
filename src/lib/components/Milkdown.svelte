9eee88f588c648e4080288f0e556ebd9483cb8d7
syntaxbullet <syntaxbullet@protonmail.com>
2023-03-27 14:16:11 +0000

    feat: implement milkdown v7 editor

---
+++ src/lib/components/Milkdown.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Editor, rootCtx } from '@milkdown/core';
+	import { commonmark } from '@milkdown/preset-commonmark';
+	import { gfm } from '@milkdown/preset-gfm';
+
+	function editor(dom: HTMLDivElement) {
+		Editor.make()
+			.config((ctx: any) => {
+				ctx.set(rootCtx, dom);
+			})
+			.use(commonmark)
+			.use(gfm)
+			.create();
+	}
+</script>
+
+<div class="editor" use:editor />
